351c07fb55bc4b2adbd6f1c4da839da33592eacb
?ystein Bj?rndal <ob@cakebox.net>
2013-12-20 10:52:38 +0100

    added setup.py for python installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='matrix2latex',
+      version='1.9',
+      description='Takes a python matrix or nested list and converts to a LaTeX table or matrix.',
+      long_description=open('README.txt').read(),
+      author='obtitus',
+      author_email='obtitus@gmail.com',
+      url='https://code.google.com/p/matrix2latex/',
+      packages=['matrix2latex'],
+  )
