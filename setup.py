700c2910ed92096524b99d05209c04e351fa275b
Horea Christian <h.chr@mail.ru>
2015-09-27 19:47:55 +0200

    adjusted README name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='matrix2latex',
       version='1.9',
       description='Takes a python matrix or nested list and converts to a LaTeX table or matrix.',
-      long_description=open('README.txt').read(),
+      long_description=open('README.md').read(),
       author='obtitus',
       author_email='obtitus@gmail.com',
       url='https://code.google.com/p/matrix2latex/',
