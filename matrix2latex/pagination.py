4df88922fb72d4d8b6f113a5455587c710c1511f
Horea Christian <horea.christ@yandex.com>
2016-03-18 03:45:05 +0100

    corrected indent

--- matrix2latex/pagination.py
+++ matrix2latex/pagination.py
@@ -17,40 +17,41 @@ from subprocess import call
 
 def simple(matrix, headerRow=None, headerColumn=None, Filename=None, font_size=None, clean_latex=True):
 	"""A simple pagination function, that creates a minimal LaTeX document code for an input matrix,
-compiles it, and removes the LaTeX traces.
+	compiles it, and removes the LaTeX traces.
 
-Arguments:
+	Arguments:
 
-matrix
-	A numpy matrix or a nested list
+	matrix
+		A numpy matrix or a nested list
 
-Filename
-	File to place output, extension .tex is added automatically. File can be included in a LaTeX
-	document by \input{filename}. Output will always be returned in a string. If filename is None
-	or not a string it is ignored.
+	Filename
+		File to place output, extension .tex is added automatically. File can be included in a LaTeX
+		document by \input{filename}. Output will always be returned in a string. If filename is None
+		or not a string it is ignored.
 
-headerRow
-	A row at the top used to label the columns.
-	Must be a list of strings. Can be a nested list for multiple headings.
-	If two or more items are repeated, a multicolumn is inserted, so:
-	headerRow=['a', 'a']
-	will produces "\multicolumn{2}{c}{Item}" with an appropriate cmidrule beneath.
-	To avoid this behavior ensure each consecutive item is unique, for instance:
-	headerRow=['a', 'a ']
-	will produces the expected "a & a".
+	headerRow
+		A row at the top used to label the columns.
+		Must be a list of strings. Can be a nested list for multiple headings.
+		If two or more items are repeated, a multicolumn is inserted, so:
+		headerRow=['a', 'a']
+		will produces "\multicolumn{2}{c}{Item}" with an appropriate cmidrule beneath.
+		To avoid this behavior ensure each consecutive item is unique, for instance:
+		headerRow=['a', 'a ']
+		will produces the expected "a & a".
 
-headerColumn
-	A column used to label the rows.
-	Must be a list of strings
+	headerColumn
+		A column used to label the rows.
+		Must be a list of strings
 
-font_size
-	Specify the global (document and table) font size.
-	Accepted values are integers from 1 to 10 - these are mapped on the available LaTeX font sizes https://en.wikibooks.org/wiki/LaTeX/Fonts
+	font_size
+		Specify the global (document and table) font size.
+		Accepted values are integers from 1 to 10 - these are mapped on the available LaTeX font sizes
+		https://en.wikibooks.org/wiki/LaTeX/Fonts
 
-clean_latex
-	Used to optionally turn off the delete phase for LaTeX traces
-	Must be bool
-"""
+	clean_latex
+		Used to optionally turn off the delete phase for LaTeX traces
+		Must be bool
+	"""
 
 	latex_font_sizes = {
 	1: "\\tiny",
